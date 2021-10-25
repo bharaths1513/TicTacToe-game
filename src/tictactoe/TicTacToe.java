9ca135a96a7e1f8da843ca7397dbed9027a80ed4
bharaths1513 <bharaths.1311@gmail.com>
2021-10-25 20:57:35 +0530

    assinging character to board

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 class tictac
 {
 	char board[] = new char[10];
-	char cross = 'x';
+	char cross ='x';
 	char zero = 'o';
 	char player;
 	char computer;
 	
-	void init()
+	public void init()
 	{
 		for(int i=1; i<board.length; i++)
 		{
@@ -19,12 +19,15 @@ class tictac
 	}
 	
 	
-	void allow_player()
+	public void allow_player()
 	{
+		
 		System.out.println("Enter X or O you want to choose");
 		Scanner n=new Scanner(System.in);
 		player = n.next().charAt(0);
-		if( player == cross)
+		
+		
+		 if( player == cross)
 		{
 			player = cross;
 			computer = zero;
@@ -38,19 +41,42 @@ class tictac
 		else
 		{
 			System.out.println("Invalied character");
+			
 		}
+		 
+		
+		
 			
 	}
 	
-	void show_board()
+	public void show_board()
 	{
-		 System.out.println("Unassigned board  :");
+		 System.out.println("game board  :");
 	        System.out.println("");
 	        System.out.println("   "+board[1]+"   " + "|" +"   "+board[2]+ "   " + "|" +"   "+board[3]+ "   ");
 	        System.out.println("  -------------------");
 	        System.out.println("   "+board[4]+"   " + "|" +"   "+board[5]+ "   " + "|" +"   "+board[6]+ "   ");
 	        System.out.println("  -------------------");
 	        System.out.println("   "+board[7]+"   " + "|" +"   "+board[8]+ "   " + "|" +"   "+board[9]+ "   ");
+	}
+	public void user_move()
+	{
+		System.out.println("Enter between(1-9) where you have to move ");
+		Scanner n=new Scanner(System.in);
+		int position = n.nextInt();
+		
+		if(position>=1 && position<=9)
+		{
+			if(board[position]== ' ' )
+			{
+				board[position] = player;
+				show_board();
+			}
+			else
+			{
+				System.out.println("Invalied move,Position is not Empty");
+			}
+		}
 	}
 }
 
@@ -63,6 +89,7 @@ public class TicTacToe {
 		o.init();
 		o.allow_player();
 		o.show_board();
+		o.user_move();
 	}
 
 }
